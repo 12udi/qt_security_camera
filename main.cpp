d001caf5f5778e91b7b5698da1288528c4a5bf21
Rudi <12udi@web.de>
2023-01-14 17:54:08 +0100

    adding opencv to app

--- main.cpp
+++ main.cpp
@@ -1,12 +1,56 @@
 #include <QtGui>
 #include <QtQml>
+#include <opencv4/opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    engine.load(QStringLiteral("qrc:/tabs/main_ui.qml"));
+//    QQmlApplicationEngine engine;
+//    engine.load(QStringLiteral("qrc:/tabs/main_ui.qml"));
 
-    return app.exec();
+//    std::string image_path = samples::findFile("starry_night.jpg");
+//    Mat img = imread(image_path, IMREAD_COLOR);
+//    if(img.empty())
+//    {
+//        std::cout << "Could not read the image: " << image_path << std::endl;
+//        return 1;
+//    }
+//    imshow("Display window", img);
+//    int k = waitKey(0); // Wait for a keystroke in the window
+//    if(k == 's')
+//    {
+//        imwrite("starry_night.png", img);
+//    }
+
+    Mat image;
+
+    namedWindow("Display window");
+
+    VideoCapture cap(2);
+
+    if (!cap.isOpened()) {
+
+    cout << "cannot open camera";
+
+    }
+
+    while (true) {
+
+    cap >> image;
+
+    imshow("Display window", image);
+
+    waitKey(25);
+
+    }
+
+    return 0;
+    return 0;
 }
