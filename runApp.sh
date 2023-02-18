0704be8c903c96a25c2475f582cb8773aac235fa
Rudi <12udi@web.de>
2023-02-18 15:37:25 +0100

    feat: update runscript for rpi

--- runApp.sh
+++ runApp.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=${PWD}/etc/lib
-#export QT_PLUGIN_PATH=${PWD}/etc/plugins
-#export QML2_IMPORT_PATH=${PWD}/etc/qml
-./build/qt-conan
+export DISPLAY=:0
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/qt6/lib/:/usr/local/lib/aarch64-linux-gnu
+export GST_PLUGIN_PATH=~/libcamera/build/src/gstreamer
+
+# Debug Mode for GSTREAM
+#GST_DEBUG=4 G_DEBUG=fatal-criticals gdb -ex run --args ./qt-cam-app &
+
+./qt-cam-app &
+python main.py
